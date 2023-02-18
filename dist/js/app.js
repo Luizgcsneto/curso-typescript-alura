0046db1b0ec0e9de4551f33db6628a0b56967809
Luiz Gonzaga <luizgonzaga15@gmail.com>
2023-02-18 14:43:22 -0300

    atualizações no projeto de TypeScript

---
+++ dist/js/app.js
@@ -0,0 +1,7 @@
+import { NegociacoesController } from './controllers/negociacoes-controller';
+const controller = new NegociacoesController();
+const form = document.querySelector('.form');
+form.addEventListener('submit', event => {
+    event.preventDefault();
+    controller.adiciona();
+});
